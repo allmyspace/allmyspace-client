8e2dc8dca355fcac185ce5d2508f7d2e7a05265e
Rajat Khanduja <rajatkhanduja13@gmail.com>
2014-05-31 16:28:10 +0530

    Updated DropboxSyncClient. Fixed errors.

--- space-providers/Dropbox/DropboxSyncClient.py
+++ space-providers/Dropbox/DropboxSyncClient.py
@@ -1,27 +1,27 @@
-from dropbox.client import DropboxClient as original_client
+from dropbox.client import DropboxClient
 
 class DropboxSyncClient:
     
-    def __init__(oauth2_access_token):
+    def __init__(self, oauth2_access_token):
         self._client = DropboxClient(oauth2_access_token) 
 
-    def upload_file(dropbox_file_path, local_file_path, replace = False):
+    def upload_file(self, dropbox_file_path, local_file_path, replace = False):
         f = open(local_file_path, 'rb')
         response = self._client.put_file(dropbox_file_path, f, replace)
         return response
 
-    def generate_public_url(dropbox_file_path):
+    def generate_public_url(self, dropbox_file_path):
         return self._client.share(path)['url']
 
-    def delete_file(dropbox_file_path):
+    def delete_file(self, dropbox_file_path):
         self._client.file_delete(dropbox_file_path)
 
-    def update_local_to_cloud(dropbox_file_path, local_file_path):
-        return upload_file(dropbox_file_path, local_file_path, True)        
+    def update_local_to_cloud(self, dropbox_file_path, local_file_path):
+        return self.upload_file(dropbox_file_path, local_file_path, True)        
 
-    def update_cloud_to_local(dropbox_file_path, local_file_path):
-        open(local_file_path, 'wb').write(self._client.get_file(dropbox_file_path))
+    def update_cloud_to_local(self, dropbox_file_path, local_file_path):
+        open(local_file_path, 'wb').write(self._client.get_file(dropbox_file_path).read())
 
-    def get_file_list(dropbox_folder_path):
+    def get_file_list(self, dropbox_folder_path):
         folder_metadata = self._client.metadata(dropbox_folder_path)
         return [content['path'] for content in folder_metadata['contents']]
